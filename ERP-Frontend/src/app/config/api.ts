80a6471b5de93fb1a2ef014c40e496f657b77fbb
unknown <GndsDev>
2026-05-27 23:49:21 -0300

    Resolvendo rotas

--- ERP-Frontend/src/app/config/api.ts
+++ ERP-Frontend/src/app/config/api.ts
@@ -1,1 +1,5 @@
-export const API_BASE_URL = 'http://localhost:8080/api';
+const isLocal = window.location.hostname === 'localhost';
+
+export const API_BASE_URL = isLocal
+  ? 'http://localhost:8080/api'
+  : 'https://abbainc-backend.onrender.com/api';
