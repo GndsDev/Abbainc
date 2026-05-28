14d195123ecb39d8be8ffdcd49624726e80200af
unknown <GndsDev>
2026-05-27 22:56:36 -0300

    Adiciona configuração real de CORS para a Vercel

--- ERP-Backend/src/main/java/com/abbainc/erp/Config/CorsConfig.java
+++ ERP-Backend/src/main/java/com/abbainc/erp/Config/CorsConfig.java
@@ -2,6 +2,7 @@ package com.abbainc.erp.Config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,5 +18,18 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         String uploadPath = Path.of(uploadDirectory).toAbsolutePath().normalize().toUri().toString();
         registry.addResourceHandler("/uploads/**").addResourceLocations(uploadPath);
+    }
+
+    // AQUI ESTÁ A MÁGICA DO CORS!
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:4200",
+                        "https://abbainc.vercel.app/"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
